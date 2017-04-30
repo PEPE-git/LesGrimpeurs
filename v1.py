0b34193f4fc8501c5f18d19c851d80c353b23abe
PEPE-git <pierre.mkt7@gmail.com>
2017-04-30 16:23:34 +0200

    Sorties dans tableaux excel + commentaires(pas fini) + Titres figures OK

--- v1.py
+++ v1.py
@@ -7,6 +7,7 @@ from math import sqrt
 import matplotlib.pyplot as plt
 from scipy.stats.stats import pearsonr
 import numpy as np
+import csv
 
 #PLAN
 	#Changements conformationnels globaux
@@ -529,11 +530,11 @@ def corEnfouissementFlexibilite_conf(d_conf) :
 # PARTIE 6 : ECRITURE DES RESULTATS
 #-----------------------------------------------------------------------
 
-def ecriture(l_dict) :
+def ecriture(l_dict,methode) :
 
-	print "Ecriture des resultats dans les fichiers de sortie 'res_barstar_globaux.txt' et 'res_barstar_locaux.txt'"
-	output1 = __verificationfFichier("res_barstar_globaux.txt") # nom de fichier par defaut, mais on ne veut pas ecraser des resultats precedents
-	output2 = __verificationfFichier("res_barstar_locaux.txt")
+	print "Ecriture des resultats dans les fichiers de sortie 'res_barstar_globaux_"+methode+".csv' et 'res_barstar_locaux_"+methode+".csv'"
+	output1 = __verificationfFichier("res_barstar_globaux_"+methode+".csv") # nom de fichier par defaut, mais on ne veut pas ecraser des resultats precedents
+	output2 = __verificationfFichier("res_barstar_locaux_"+methode+".csv")
 	
 	x = input("Indiquez le nombre de decimales souhaitees :\t")
 	__outputGlobaux(output1, l_dict[0], l_dict[1], x)
@@ -542,7 +543,7 @@ def ecriture(l_dict) :
 
 
 def __verificationfFichier(output) :
-	
+	#Si le fichier existe deja, demande si l'utilisateur veut le remplacer ou non
 	if (os.path.exists(output)) :
 		decision = raw_input("Fichier de sortie "+str(output)+" deja existant : Voulez vous l'ecraser ? O/N\n")
 	
@@ -558,61 +559,66 @@ def __verificationfFichier(output) :
 
 
 def __outputGlobaux(output, d_ref, d_conf, x) :
+	#Ecriture des resultats dans un tableau excel
+
 	try:
-		f = open(output, "w")
-		
-		texte = "Conf|\tRayon Giration\t|\tDistance\t\t|\t\tRMSD\t\t|\tRatio Giration\t|\tCorrelation\t(p-value)\n"
+		with open(output, "w") as f:
+			fieldnames = ['Conf', 'Rayon Giration','Distance','ecart-type Distance','RMSD','ecart-type RMSD','Ratio Giration','Correlation','p-value Correlation']
+			writer = csv.DictWriter(f, fieldnames=fieldnames)
 
-		rayonG_ref = d_ref["rayonGiration"][0]
-		texte += "REF\t|\t"+str(round(rayonG_ref,x))+"\t\t|\t\t\t0\n"
-		for i in range(len(d_conf["liste_conformations"])) :
-			num = d_conf["liste_conformations"][i].strip()
-			rayonG = d_conf["rayonGiration"][i]
-			d_moy = d_conf["distance_moy"][i]
-			d_sd = d_conf["distance_sd"][i]
-			rmsd = d_conf["RMSDmoy"][i]
-			rmsd_sd = d_conf["RMSDmoy_sd"][i]
-			ratio_gir = d_conf["ratio_giration"][i]
-			cor = d_conf["corEnfFlexi_conf"][0][i]
-			pvalue = d_conf["corEnfFlexi_conf"][1][i]
-			texte += str(num)+"\t|\t"+str(round(rayonG,x))+"\t\t|\t"+str(round(d_moy,x))+"\t(+/-"+str(round(d_sd,x))+")\t|\t"+str(round(rmsd,x))+"\t(+/-"+str(round(rmsd_sd,x))+")\t|\t"+str(round(ratio_gir,x))+"\t\t|\t"+str(round(cor,x))+"\t("+str(round(pvalue,x))+")\n"
+			writer.writeheader()
+			writer.writerow({'Conf':'REF','Rayon Giration': round(d_ref["rayonGiration"][0],x), 'Distance': 0})
 
-		f.write(texte)
-		f.close()		
+			for i in range(len(d_conf["liste_conformations"])) :
+
+				num = d_conf["liste_conformations"][i].strip()
+				rayonG = d_conf["rayonGiration"][i]
+				d_moy = d_conf["distance_moy"][i]
+				d_sd = d_conf["distance_sd"][i]
+				rmsd = d_conf["RMSDmoy"][i]
+				rmsd_sd = d_conf["RMSDmoy_sd"][i]
+				ratio_gir = d_conf["ratio_giration"][i]
+				cor = d_conf["corEnfFlexi_conf"][0][i]
+				pvalue = d_conf["corEnfFlexi_conf"][1][i]
+				writer.writerow({'Conf': num, 'Rayon Giration': round(rayonG,x), 'Distance': round(d_moy,x), 'ecart-type Distance': round(d_sd,x), 'RMSD': round(rmsd,x), 'ecart-type RMSD': round(rmsd_sd,x), 'Ratio Giration': round(ratio_gir,x), 'Correlation': round(cor,x),'p-value Correlation': round(pvalue,x)})	
 
 	except:
-		print("Erreur chargement fichier global\n")
+		print("Erreur chargement fichier"+output+"\n")
 		sys.exit(0)
 
+
 def __outputLocaux(output, d_ref, x) :
-	
+	#Ecriture des resultats dans un tableau excel
+
 	try:
-		f = open(output, "w")
+		with open(output, "w") as f:
+			fieldnames = ['Residus', 'Nom','RMSD', 'ecart-type RMSD','Distance residu/CdM','ecart-type Distance residu/CdM']
+			writer = csv.DictWriter(f, fieldnames=fieldnames)
 		
-		texte = "Residus\t|\t Nom \t|\tRMSD (+/- ecart-type)\t|\tDistance residu/CdM (+/- ecart-type)\n"
-		conf_ref = d_ref["liste_conformations"][0]
-		lres = d_ref[conf_ref]["liste_n_residus"]
+			writer.writeheader()
+
+			conf_ref = d_ref["liste_conformations"][0]
+			lres = d_ref[conf_ref]["liste_n_residus"]
 		
-		for i in range(len(lres)) :
-			res = lres[i]
-			nom = d_ref[d_ref["liste_conformations"][0]]["liste_seq_residus"][i]
-			rmsd = d_ref["RMSDres_mean"][i]
-			rmsd_sd = d_ref["RMSDres_sd"][i]
-			d = d_ref["enfRes_mean"][i]
-			d_sd = d_ref["enfRes_sd"][i]
+			for i in range(len(lres)) :
+				res = lres[i]
+				nom = d_ref[d_ref["liste_conformations"][0]]["liste_seq_residus"][i]
+				rmsd = d_ref["RMSDres_mean"][i]
+				rmsd_sd = d_ref["RMSDres_sd"][i]
+				d = d_ref["enfRes_mean"][i]
+				d_sd = d_ref["enfRes_sd"][i]
 			
-			texte += str(res)+"\t\t|\t"+nom+"\t\t|\t"+str(round(rmsd,x))+"\t(+/-"+str(round(rmsd_sd,x))+")\t\t|\t"+str(round(d,x))+"\t(+/-"+str(round(d_sd,x))+")\n"
-			
-		f.write(texte)
-		f.close()
-		
+				writer.writerow({'Residus': res, 'Nom': nom, 'RMSD': round(rmsd,x),'ecart-type RMSD': round(rmsd_sd,x), 'Distance residu/CdM': round(d,x), 'ecart-type Distance residu/CdM' : round(d_sd,x)})	
+
+				
 	except:
-		print("Erreur chargement fichier local\n")
+		print("Erreur chargement fichier"+output+"\n")
 		sys.exit(0)
 
 #-----------------------------------------------------------------------
 # GRAPHIQUES
 #-----------------------------------------------------------------------
+
 def plotRes(l_dict) :
 	plotGlobal(l_dict[1])
 	plotLocal(l_dict[0]) 
@@ -694,16 +700,18 @@ def plotGlobalRMSD(d_conf) :
 
 	plt.show()
 	
+
+
 def plotFlexibiteEnfouissement(d_conf) :
 	plt.subplot(211)
 	plt.plot(d_conf["corEnfFlexi_conf"][0])
-	plt.title('Correlation entre la Flexibilite du residu\n et son Enfouissement, en fonction des conformations')
+	plt.title('Correlation de la Flexibilite moyenne des residus\n et leur Enfouissement moyen, en fonction des conformations')
 	plt.xlabel('Conformations')
 	plt.ylabel('Correlation')
 	
 	plt.subplot(212)
 	plt.plot(d_conf["corEnfFlexi_conf"][1])
-	plt.title('p-value de la Correlation Flexibilite/Enfouissement\n en fonction des conformations')
+	plt.title('p-value de la Correlation Flexibilite/Enfouissement moyens \n en fonction des conformations')
 	plt.xlabel('Conformations')
 	plt.ylabel('p-valeur')
 	
@@ -773,8 +781,8 @@ def plotRMSDLocal(d_ref) :
 if __name__ == '__main__':
 	liste_dictionnaire = dictionnaire()					# Creation d'une liste : [dictionnaire_des_conformations,dictionnaire_de_la_reference]
 	centreMasse = choixMeth()							# "CM_CA" ou "CM_moyall" en fonction de la methode de calcul choisie
-	conformation_analysis(liste_dictionnaire) 			# calcul des variables d'interet
-	ecriture(liste_dictionnaire) 						# ecriture dans un fichier
-	plotRes(liste_dictionnaire) 						# graphiques
+	conformation_analysis(liste_dictionnaire) 			# calcul des variables d'interet et ajout dans les dictionnaires 
+	ecriture(liste_dictionnaire,centreMasse) 			# ecriture des resultats dans les tableaux excel de sortie, en fonction de la methode de calcul des CdM
+	plotRes(liste_dictionnaire) 						# representations graphiques
 
 
